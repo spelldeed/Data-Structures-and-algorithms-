c5bf2b4c40645bc9d34df5e7d6a5a04b2df3f81c
spelldeed <70479251+spelldeed@users.noreply.github.com>
2021-03-20 22:27:20 +0530

    Add files via upload

---
+++ Data Structures and algorithms code/Dynamic Programming/RangeSumQuery.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#define ll long long int
+#define fo(i,a,n,m) for(ll i=a;i<=(n-1);i+=m)
+using namespace std;
+ll n;
+vector<ll> memo(n+1,0);
+
+ll sumRange(ll left, ll right)
+{
+    if (left == 0)
+        return(memo[right]);
+    else
+        return(memo[right] - memo[left-1]);
+}
+
+int main()
+{
+	ll t;
+	cin >> n;
+    vector<ll> nums(n);
+    fo(i, 0, n, 1)
+    {
+        cin >> nums[i];
+        if (i == 0)
+            memo[0] = nums[0];
+        if (i != 0)
+            memo[i] = memo[i - 1] + nums[i];
+        
+    }
+    fo(i, 0, n, 1)
+    {
+        cout << memo[i] << " ";
+    }
+    cout << "\n";
+    cin >> t;
+	while (t--)
+	{
+		ll a, b;
+		cin >> a >> b;
+	    
+		cout << sumRange(a, b) << "\n";
+	}
+}
